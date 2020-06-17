6a4892e1cfe8c7fa1452098c477d0b894f215746
jiyi666 <1003175215@qq.com>
2020-06-17 17:52:15 +0800

    [first commit]FinancialFreedom
    Signed-off-by: jiyi666 <1003175215@qq.com>

---
+++ FinancialFreedom/app/src/main/java/com/example/financialfreedom/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.financialfreedom
+
+import android.os.Bundle
+import com.example.financialfreedom.utils.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_data)
+    }
+}
